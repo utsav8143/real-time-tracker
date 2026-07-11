1322c8ae17153c746d8139128738c57a0e5ace39
utsav8143 <utsav8143@gmail.com>
2026-07-11 19:30:29 +0530

    first commit

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ import app from "./src/app.js";
 import config from "./src/config/config.js";
 import { Server } from "socket.io";
 import {createServer} from "http"
-
+import express from "express";
+import path from "path";
 
 const server=createServer(app)
 const io=new Server(server)
 
 
+
 server.listen(process.env.PORT,()=>{
     console.log("Server running on port",process.env.PORT)
 })
+
+app.set("view engine","ejs");
+app.set(express.static(path.join(__dirname,"public")));
 
 app.get("/",(req,res)=> {
     res.send("HEY")
